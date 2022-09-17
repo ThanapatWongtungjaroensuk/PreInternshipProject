ffc9008c94122e6487b3d44b17eec75ef199ee2c
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2022-09-18 01:16:28 +0700

    [ Update ] HomeScreen & data file

--- screen/homeScreen.js
+++ screen/homeScreen.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet,SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet,SafeAreaView, ScrollView } from 'react-native'
 import { useTheme } from '@react-navigation/native';
 
 function HomeScreen() {
   const { colors } = useTheme();
   return (
-    <SafeAreaView style={[styles.container, {color: colors.background}]}>
+    <SafeAreaView style={[styles.container,{color: colors.background}]}>
       <StatusBar/>
-      <Text style={{fontSize: 25, color: colors.text}}>Home page</Text>
+      <View style={styles.headerBar}>
+        <Text style={{fontSize: 25, color: colors.text, fontWeight: '500'}}>ToonView</Text>
+      </View>
+      <ScrollView style={styles.detailscreen}>
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -18,7 +23,14 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  headerBar: {
+    backgroundColor: 'orange',
+    marginTop: 40,
+    padding: 10
+  },
+  detailscreen: {
+    flex: 1,
+    //backgroundColor: 'black',
   },
 });
